2fdc4e4b663cd9d7dc63d63fd970937ae8098655
Thais Vale <126836029+tthaisvale@users.noreply.github.com>
2025-03-25 20:01:53 -0300

    Add files via upload

---
+++ Backend3-main/tecback-master/src/main/java/br/uniesp/si/techback/service/FilmeService.java
@@ -0,0 +1,42 @@
+package br.uniesp.si.techback.service;
+
+import br.uniesp.si.techback.model.Filme;
+import br.uniesp.si.techback.repository.FilmeRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Service
+public class FilmeService {
+
+    @Autowired
+    private FilmeRepository repository;
+
+    public Filme cadastrar(Filme filme){
+        return repository.save(filme);
+    }
+
+    public Filme alterar(Filme filme){
+        if(filme.getId()==null){
+            throw new EntityNotFoundException();
+        }
+        return repository.save(filme);
+    }
+
+    public List<Filme> listar (){
+        return repository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Integer){
+        service.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+}
